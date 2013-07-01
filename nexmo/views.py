ed29b03e7b3053585be0ea2a18207dce651d00c9
Thibault Jouannic <thibault@miximum.fr>
2013-07-01 17:43:17 +0200

    was using POST instead of GET

--- nexmo/views.py
+++ nexmo/views.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 def callback(request):
     """Callback URL for Nexmo."""
-    message_id = request.POST.get('messageId')
-    status_id = int(request.POST.get('status'))
+    message_id = request.GET.get('messageId')
+    status_id = request.GET.get('status')
     status_msg = NEXMO_STATUSES.get(status_id, UNKNOWN_STATUS)
-    error_id = int(request.POST.get('err-code'))
+    error_id = int(request.GET.get('err-code'))
     error_msg = NEXMO_MESSAGES.get(error_id, UNKNOWN_MESSAGE)
 
     logger.info(u'Nexmo callback: Sms = %s, Status = %s, message = %s' % (
