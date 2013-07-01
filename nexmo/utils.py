7613285ba24990d62cf2273387a143aa74ce8bb0
Thibault Jouannic <thibault@miximum.fr>
2013-07-01 16:00:53 +0200

    add shortcut to send sms message

---
+++ nexmo/utils.py
@@ -0,0 +1,23 @@
+from .libpynexmo.nexmomessage import NexmoMessage
+from django.conf import settings
+
+
+def send_message(to, message):
+    """Shortcut to send a sms using libnexmo api.
+
+    Usage:
+
+    >>> from nexmo import send_message
+    >>> send_message('+33612345678', 'My sms message body')
+    """
+    params = {
+        'username': settings.NEXMO_USERNAME,
+        'password': settings.NEXMO_PASSWORD,
+        'type': 'unicode',
+        'from': settings.NEXMO_FROM,
+        'to': to,
+        'text': message.encode('utf-8'),
+    }
+    sms = NexmoMessage(params)
+    response = sms.send_request()
+    return response
