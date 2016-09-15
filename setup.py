f970e3be9eadc5b38ec331c634271311729e01dc
Thibault Jouannic <thibault@miximum.fr>
2016-09-15 16:05:49 +0200

    Rename package nexmo to djexmo
    We will use the official nexmo lib instead of the insupported libnexmo.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nexmo',
-    version='1.0.2',
-    packages=['nexmo'],
+    version='2.0.0',
+    packages=['djexmo'],
     include_package_data=True,
     license='MIT',
     description='A simple Django app to send text messages using the Nexmo api.',
