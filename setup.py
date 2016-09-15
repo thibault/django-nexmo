b548d6a38b76e45d785418ee7e5883e7cb315251
Thibault Jouannic <thibault@miximum.fr>
2016-09-15 17:01:55 +0200

    Make the latest version an alpha

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nexmo',
-    version='2.0.0',
+    version='2.0.0-alpha1',
     packages=['djexmo'],
     include_package_data=True,
     license='MIT',
